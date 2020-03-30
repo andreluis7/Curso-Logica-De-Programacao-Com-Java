49cb6cac8fc1e89f53d2bd5043408875a3a43ffb
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-03-29 21:36:33 -0300

    calculo do quadrado

---
+++ 03.04-exercicio/src/IntroducaoVariaveis.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IntroducaoVariaveis {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite um número: ");
+		Double numero = scanner.nextDouble();
+		
+		Double quadrado = numero * numero;
+		
+		System.out.println("O quadrado do numero " + numero + " é : " + quadrado);
+		
+		scanner.close();
+	}
+
+}
