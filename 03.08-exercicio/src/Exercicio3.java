2d64bae3b1d8659d4322ca4de3b02e1434edb363
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-04-04 10:32:44 -0300

    Tipo lógico - verificacao de nota para aprovacao

---
+++ 03.08-exercicio/src/Exercicio3.java
@@ -0,0 +1,16 @@
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		
+		Integer nota = 2;
+
+		Boolean podeTirarCarteira = nota >= 5;
+
+		if (podeTirarCarteira) {
+			System.out.println("O aluno(a) passou de ano.");
+		} else {
+			System.out.println("O aluno(a) não passou de ano.");
+		}
+	}
+
+}
