5f8bccf51a3ee53af3ab5564cda2a4bcaa822764
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-04-04 10:36:13 -0300

    Update Exercicio 3
    implementando a opcao do usuario digitar a nota

--- 03.08-exercicio/src/Exercicio3.java
+++ 03.08-exercicio/src/Exercicio3.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Exercicio3 {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite a sua nota: ");
+		Double notaDoAluno = scanner.nextDouble();
 		
-		Integer nota = 2;
-
-		Boolean podeTirarCarteira = nota >= 5;
-
-		if (podeTirarCarteira) {
+		Boolean passouDeAno = notaDoAluno >= 7;
+		
+		if (passouDeAno) {
 			System.out.println("O aluno(a) passou de ano.");
 		} else {
 			System.out.println("O aluno(a) não passou de ano.");
